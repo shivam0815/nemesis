df0f00b49d65061051029bb0bc99b3564c096c0f
Shivam Shukla <shivamshukla8103@gmail.com>
2025-12-30 18:17:31 +0530

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+
+  server: {
+    allowedHosts: [
+      "localhost",
+      "127.0.0.1",
+      "nemesis-gt3h.onrender.com", // ✅ Render backend
+    ],
+  },
+
+  preview: {
+    allowedHosts: [
+      "localhost",
+      "127.0.0.1",
+      "nemesis-gt3h.onrender.com", // ✅ Render backend
+    ],
+  },
+});
