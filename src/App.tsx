de387d21eaa1fdfefb483e5de1d6f9419da9da42
Shivam Shukla <shivamshukla8103@gmail.com>
2025-12-30 18:01:43 +0530

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import Services from "./pages/Services";
 import HRTraining from "./pages/HRTraining";
 import CustomerTraining from "./pages/CustomerTraining";
